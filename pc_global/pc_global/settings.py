3be455535b508fa7e4552c797fdab521aa753f01
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-08-26 20:39:10 -0500

    Creacion y registro apps

--- pc_global/pc_global/settings.py
+++ pc_global/pc_global/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'facturation',
+    'inventory',
+    'pqrs'
 ]
 
 MIDDLEWARE = [
