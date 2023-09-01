a1c9abe6b959eba23d1c417c63f57892ef823071
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-01 07:37:32 -0500

    Traduccion Nombre Apps

--- pc_global/inventory/apps.py
+++ pc_global/inventory/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
+
+    # Configurar Nombre Personalizado
+    verbose_name="Inventario"
