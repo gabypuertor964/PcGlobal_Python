f70f3dfcc39fed2c62fbbe99be98904011703c1b
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-04 14:31:45 -0500

    Add Hosts

--- pc_global/pc_global/settings.py
+++ pc_global/pc_global/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-anxxo-(u5@mrx42=1%o)++$8)s12^sr+=!51_8%u$$epk8exvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'jazzmin',
