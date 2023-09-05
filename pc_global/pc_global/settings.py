7173090d7c5b7b8842092fe26a38c0d610e89e19
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-05 08:50:16 -0500

    Desactivate Jazzmin

--- pc_global/pc_global/settings.py
+++ pc_global/pc_global/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
-    'jazzmin',
+    #'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
