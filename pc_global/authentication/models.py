69ccdbb45d58e61bcf44036df177bef0f1bcf7bf
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-08 20:12:49 -0500

    Guardado CSV e importacion en panel

--- pc_global/authentication/models.py
+++ pc_global/authentication/models.py
@@ -143,7 +143,12 @@ class UserCustom(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+
+        # Si no tiene nombre ni apellido, se mostrara el username
+        if(self.first_name == "" and self.last_name == ""):
+            return self.username
+        else:
+            return f"{self.first_name} {self.last_name}"
     
     # Metadatos de la tabla
     class Meta:
--- pc_global/landing/admin.py
+++ pc_global/landing/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from landing.models import Areas,States
 
 # Register your models here.
-admin.site.register(Areas)
-admin.site.register(States)
+@admin.register(Areas)
+class AreasAdmin(ImportExportModelAdmin):
+    
+    #Campos a mostrar
+    list_display = ("id", "nombre")
+
+    #Campos empleados como link a la pagina de modificacion
+    list_display_links = ("id",)
+
+    #Items editables en el panel (Input)
+    list_editable = ("nombre",)
+
+    #Campos por los que se puede buscar
+    search_fields = ("nombre",)
+
+@admin.register(States)
+class StatesAdmin(ImportExportModelAdmin):
+    
+    #Campos a mostrar
+    list_display = ("id", "nombre", "id_area")
+
+    #Campos empleados como link a la pagina de modificacion
+    list_display_links = ("id",)
+
+    #Items editables en el panel (Input)
+    list_editable = ("nombre","id_area")
+
+    #Campos por los que se puede buscar
+    search_fields = ("nombre","id_area")
