09e5f6580d2e2de1f015e361afe8d743bed57c4a
GABY PUERTO <88343868+gabypuertor964@users.noreply.github.com>
2023-09-11 23:15:09 -0500

    Restablecimiento parcial de contraseña

--- pc_global/authentication/urls.py
+++ pc_global/authentication/urls.py
@@ -5,6 +5,7 @@ from .views import RegisterView, LoginView
 
 # Import Auth Views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import Email
 
 urlpatterns = [
 
@@ -15,4 +16,12 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
